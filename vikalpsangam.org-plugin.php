bf410892b4c374898cba037b7dbd9b5b1a0fc5b9
Martin Moxon <moxon6@googlemail.com>
2020-12-05 00:53:29 +0000

    Rename root plugin file

--- vikalpsangam.org-plugin.php
+++ vikalpsangam.org-plugin.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name:     Vikalpsangam Blocks
- * Description:     Blocks for vikalpsangam.org
+ * Plugin Name:     Vikalpsangam Plugin
+ * Description:     Plugin for vikalpsangam.org
  * Version:         0.1.0
  * Author:          Martin Moxon
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:     vikalpsangam-map
+ * Text Domain:     vikalpsangam.org
  *
- * @package         vikalpsangam-blocks
+ * @package         vikalpsangam.org-plugin
  */
 
 $dir = dirname( __FILE__ );
