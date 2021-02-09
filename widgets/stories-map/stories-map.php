e5177a076114afd8be54438f587e61aeaf37e645
Martin Moxon <moxon6@googlemail.com>
2021-02-09 14:43:49 +0000

    Simplify StoriesMapWidget

--- widgets/stories-map/stories-map.php
+++ widgets/stories-map/stories-map.php
@@ -5,20 +5,16 @@ namespace Vikalpsangam\Plugin\Widgets;
 class StoriesMapWidget extends \WP_Widget {
 
     function __construct() {
-        parent::__construct( false, __( 'Vikalpsangam Stories Map Widget', 'textdomain' ) );
+        parent::__construct( 'StoriesMapWidget', __( 'Vikalpsangam Stories Map Widget', 'textdomain' ) );
     }
 
     function widget( $args, $instance ) {
         \Timber::render("stories-map.twig");
     }
 
-    function update( $new_instance, $old_instance ) {
-        return $new_instance;
-    }
-
     function form( $instance ) {
         \Timber::render("stories-map.twig");
-        return $instance;
+        return 'noform';
     }
 }
  
