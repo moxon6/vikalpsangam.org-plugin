8226c81f57fceb5e6cdf0178b461b6f86c8c9ded
Martin Moxon <moxon6@googlemail.com>
2021-02-09 14:17:15 +0000

    Remove comments endpoint

--- widgets/stories-map/endpoint.php
+++ widgets/stories-map/endpoint.php
@@ -48,32 +48,6 @@ function register_map_endpoint() {
 	));
 }
 
-add_filter( 'rest_allow_anonymous_comments', '__return_true' ); 
-
-function register_comments_endpoint() {
-
-    function get_comments_rest($request) {       
-        global $post;
-        $post = get_post($request["post_id"]);
-        setup_postdata($post);
-
-        $response = new WP_REST_Response(
-            Timber::compile(
-                'partials/_comments.twig', 
-                [ "post" => new Timber\Post($post)]
-            )
-        );
-        $response->set_status(200);
-        return $response;
-    }
-
-	register_rest_route( 'vikalpsangam/v1', 'comments/(?P<post_id>\d+)', array(
-		'methods'  => 'GET',
-		'callback' => 'get_comments_rest'
-	));
-}
-
 add_action('rest_api_init', function () {
     register_map_endpoint();
-    register_comments_endpoint();
 });
--- widgets/stories-map/stories-map.php
+++ widgets/stories-map/stories-map.php
@@ -2,10 +2,6 @@
 
 namespace Vikalpsangam\Plugin\Widgets;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 class StoriesMapWidget extends \WP_Widget {
 
     function __construct() {
@@ -13,7 +9,7 @@ class StoriesMapWidget extends \WP_Widget {
     }
 
     function widget( $args, $instance ) {
-        Timber::render("stories-map.twig");
+        \Timber::render("stories-map.twig");
     }
 
     function update( $new_instance, $old_instance ) {
@@ -21,7 +17,7 @@ class StoriesMapWidget extends \WP_Widget {
     }
 
     function form( $instance ) {
-        Timber::render("stories-map.twig");
+        \Timber::render("stories-map.twig");
         return $instance;
     }
 }
