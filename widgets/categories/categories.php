26cb59cd12fc1fe1e9892284d647495bced24fa7
Martin Moxon <moxon6@googlemail.com>
2021-02-09 19:17:22 +0000

    Refactor categories widget further

--- widgets/categories/categories.php
+++ widgets/categories/categories.php
@@ -8,7 +8,7 @@ class VikalpsangamCategories extends \WP_Widget {
         parent::__construct( 'VikalpsangamCategories', __( 'Vikalpsangam Categories Widget', 'textdomain' ) );
     }
 
-    private function getContext() {
+    private function getContext($instance) {
         $context = \Timber::context();
         $context["post_categories"] = \Timber::get_terms('category', [
 			'hide_empty' => 1,
@@ -16,24 +16,28 @@ class VikalpsangamCategories extends \WP_Widget {
                 get_cat_ID("Perspectives"),
                 get_cat_ID("Uncategorised")
             ]
-		]);
+        ]);
+
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+        $title = esc_attr( $title );
+
+        $context["title"] = $title;
 		return $context;
     }
 
     function widget( $args, $instance ) {
-        \Timber::render("categories.twig", $this->getContext());
+        \Timber::render("categories.twig", $this->getContext($instance));
     }
 
-	public function form( $instance ) {
+    public function form( $instance ) {
         \Timber::render("categories-form.twig", array_merge(
-            $this->getContext(), 
+            $this->getContext($instance), 
             [
-                "title" => $instance['title'],
                 "title_field_id" => $this->get_field_id( 'title' ),
                 "title_field_name" => $this->get_field_name( 'title' )
             ]
         ));
-    }
+	}
 }
 
 add_action( 'widgets_init', function() {
