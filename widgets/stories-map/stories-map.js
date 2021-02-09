ed1109ea075fce1332b502808008e39f84c3eeaf
Martin Moxon <moxon6@googlemail.com>
2021-02-09 14:44:00 +0000

    Simplify stories-map.js

--- widgets/stories-map/stories-map.js
+++ widgets/stories-map/stories-map.js
@@ -1,22 +1,29 @@
+const INITIAL_ZOOM_LEVEL = 5;
+
+const mean = (arr) => arr.reduce((x, y) => x + y, 0) / arr.length;
+
+// Required for licensing purposes
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'; 
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+async function fetchCoordinates() {
+  const response = await fetch('/wp-json/vikalpsangam/v1/map');
+  const coordinatesMap = await response.json();
+  return Object.values(coordinatesMap);
+}
+
 async function renderMap(id) {
-    const mean = (arr) => arr.reduce((x, y) => x + y, 0) / arr.length;
-  
-    async function fetchCoordinates() {
-      const response = await fetch('/wp-json/vikalpsangam/v1/map');
-      const coordinatesMap = await response.json();
-      return Object.values(coordinatesMap);
-    }
-  
-    const INITIAL_ZOOM_LEVEL = 5;
     const coordinates = await fetchCoordinates();
-  
-    const map = L.map(id).setView([
+    const mapCenter = [
       mean(coordinates.map((x) => x.latitude)),
       mean(coordinates.map((x) => x.longitude)),
-    ], INITIAL_ZOOM_LEVEL);
+    ]
   
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    const map = L.map(id).setView(mapCenter, INITIAL_ZOOM_LEVEL);
+  
+    L.tileLayer(tileUrl, {
+      attribution,
     }).addTo(map);
   
     const markers = L.markerClusterGroup();
