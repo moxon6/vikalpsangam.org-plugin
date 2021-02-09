57ebd31c3946d28d9ad678eccc3600efeafc358e
Martin Moxon <moxon6@googlemail.com>
2021-02-09 16:20:30 +0000

    Create Categories Widget

--- widgets/categories/categories.php
+++ widgets/categories/categories.php
@@ -8,12 +8,24 @@ class VikalpsangamCategories extends \WP_Widget {
         parent::__construct( 'VikalpsangamCategories', __( 'Vikalpsangam Categories Widget', 'textdomain' ) );
     }
 
+    private function getContext() {
+        $context = \Timber::context();
+        $context["post_categories"] = \Timber::get_terms('category', [
+			'hide_empty' => 1,
+			"exclude" => [
+                get_cat_ID("Perspectives"),
+                get_cat_ID("Uncategorised")
+            ]
+		]);
+		return $context;
+    }
+
     function widget( $args, $instance ) {
-        \Timber::render("categories.twig");
+        \Timber::render("categories.twig", $this->getContext());
     }
 
     function form( $instance ) {
-        \Timber::render("categories.twig");
+        \Timber::render("categories.twig", $this->getContext());
         return 'noform';
     }
 }
