67881d6554119729eddf5aca51a248802fedfc79
Martin Moxon <moxon6@googlemail.com>
2021-03-22 00:37:38 +0000

    Update versioning script

--- vikalpsangam.org-plugin.php
+++ vikalpsangam.org-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Vikalpsangam Plugin
  * Description:     Plugin for vikalpsangam.org
- * Version:         0.1.0
+ * Version:
  * Author:          Martin Moxon
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
