36963494845e25f432a4ffb27a4e6acb9015e6cf
Martin Moxon <moxon6@googlemail.com>
2021-03-22 14:37:02 +0000

    Make categories selectable

--- widgets/categories/widget.php
+++ widgets/categories/widget.php
@@ -10,33 +10,32 @@ class VikalpsangamCategories extends \WP_Widget {
 
     private function getContext($instance) {
         $context = \Timber::context();
-        $context["post_categories"] = \Timber::get_terms('category', [
-			'hide_empty' => 1,
-			"exclude" => [
-                get_cat_ID("Perspectives"),
-                get_cat_ID("Uncategorised")
-            ]
-        ]);
 
-        $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
-        $title = esc_attr( $title );
+        $context["title"] = $instance['title'];
+        $context["title_field_id"] = $this->get_field_id( 'title' );
+        $context["title_field_name"] = $this->get_field_name( 'title' );
 
-        $context["title"] = $title;
+        $categories = \Timber::get_terms('category');
+        
+        $context["categories_with_meta"] = array_map(
+            fn($category) => [
+                "category" => $category,
+                "field_id" => $this->get_field_id( "category-" . $category->ID ),
+                "field_name" => $this->get_field_name( "category-" . $category->ID ),
+                "selected" => $instance["category-" . $category->ID]
+            ],
+            $categories
+        );
+
 		return $context;
     }
 
     function widget( $args, $instance ) {
         \Timber::render("templates/categories.twig", $this->getContext($instance));
     }
-
+    
     public function form( $instance ) {
-        \Timber::render("templates/categories-admin.twig", array_merge(
-            $this->getContext($instance), 
-            [
-                "title_field_id" => $this->get_field_id( 'title' ),
-                "title_field_name" => $this->get_field_name( 'title' )
-            ]
-        ));
+        \Timber::render("templates/categories-admin.twig", $this->getContext($instance));
 	}
 }
 
